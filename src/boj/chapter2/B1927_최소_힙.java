37fd8d35ab71da428c3b33d985f744e5e3fac869
jhhong0930 <jhhong0930@gmail.com>
2021-12-09 22:33:07 +0900

    최소 힙

---
+++ src/boj/chapter2/B1927_최소_힙.java
@@ -0,0 +1,29 @@
+package boj.chapter2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class B1927_최소_힙 {
+
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    public static void solution(int num) {
+
+        if (num == 0) {
+            if (pq.isEmpty()) System.out.println(0);
+            else System.out.println(pq.remove());
+        } else pq.add(num);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<n; i++) {
+            solution(Integer.parseInt(br.readLine()));
+        }
+    }
+}

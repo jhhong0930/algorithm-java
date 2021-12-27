e56041f023ec8429f7d5e4d85a1c97f04f5fc99d
jhhong0930 <jhhong0930@gmail.com>
2021-12-27 21:19:19 +0900

    크레인 인형뽑기

---
+++ src/inflearn/section5/P3_크레인_인형뽑기.java
@@ -0,0 +1,49 @@
+package inflearn.section5;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class P3_크레인_인형뽑기 {
+
+    public static int solution(int[][] board, int[] moves) {
+
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int pos : moves) {
+            for (int i=0; i< board.length; i++) {
+                if (board[i][pos - 1] != 0) {
+                    int temp = board[i][pos - 1];
+                    board[i][pos - 1] = 0;
+                    if (!stack.isEmpty() && temp == stack.peek()) {
+                        answer += 2;
+                        stack.pop();
+                    }
+                    else stack.push(temp);
+                    break;
+                }
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] board = new int[n][n];
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+        int m = sc.nextInt();
+        int[] moves = new int[m];
+
+        for (int i=0; i<m; i++) moves[i] = sc.nextInt();
+        System.out.println(solution(board, moves));
+    }
+}

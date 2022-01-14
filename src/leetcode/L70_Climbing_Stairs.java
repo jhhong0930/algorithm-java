0926d77a08079b90590d0796548d789ff8a97067
jhhong0930 <jhhong0930@gmail.com>
2022-01-14 19:21:58 +0900

    Climbing Stairs

--- src/leetcode/L70_Climbing_Stairs.java
+++ src/leetcode/L70_Climbing_Stairs.java
@@ -2,18 +2,35 @@ package leetcode;
 
 public class L70_Climbing_Stairs {
 
-    public int climbStairs(int n) {
+//    public static int climbStairs(int n) {
+//
+//        int mem1 = 1;
+//        int mem2 = 2;
+//
+//        if (n < 3) return n;
+//
+//        for (int i=3; i<=n; i++){
+//            mem2 = mem1 + mem2;
+//            mem1 = mem2 - mem1;
+//        }
+//
+//        return mem2;
+//    }
 
-        int mem1 = 1;
-        int mem2 = 2;
+    public static int climbStairs(int n) {
 
-        if( n < 3) return n;
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-        for( int i = 3 ; i <= n ; i++){
-            mem2 = mem1 + mem2;
-            mem1 = mem2-mem1;
-        }
+        for (int i=2; i<=n; i++) dp[i] = dp[i - 1] + dp[i - 2];
+        return dp[n];
+    }
 
-        return mem2;
+    public static void main(String[] args) {
+
+        System.out.println(climbStairs(2));
+        System.out.println(climbStairs(3));
+        System.out.println(climbStairs(10));
     }
 }

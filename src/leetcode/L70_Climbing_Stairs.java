62206f3a32e5b6d9d55036ddcc5cc61d01002866
jhhong0930 <jhhong0930@gmail.com>
2022-01-15 15:54:41 +0900

    Climbing Stairs

--- src/leetcode/L70_Climbing_Stairs.java
+++ src/leetcode/L70_Climbing_Stairs.java
@@ -19,7 +19,7 @@ public class L70_Climbing_Stairs {
 
     public static int climbStairs(int n) {
 
-        int dp[] = new int[n + 1];
+        int[] dp = new int[n + 1];
         dp[0] = 1;
         dp[1] = 1;
 
